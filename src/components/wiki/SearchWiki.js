3fbbc47680d9e0709ef09b5c146d61823c588d9d
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2022-02-21 23:01:53 +0530

    Fixing Sumbit

--- src/components/wiki/SearchWiki.js
+++ src/components/wiki/SearchWiki.js
@@ -4,15 +4,22 @@ const SearchWiki = ({ setSearchTerm, searchTerm }) => {
   const onChange = (term) => {
     setSearchTerm(term); // Passes Search term to parent component
   };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.search.value);
+  };
+
   return (
     <React.Fragment>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           name="search"
           type="text"
           value={searchTerm}
           onChange={(e) => onChange(e.target.value)}
         />
+        <button type="submit">Search</button>
       </form>
     </React.Fragment>
   );
