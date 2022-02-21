644b9800f6279dc29c4627ebed3170a52fbd07dc
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2022-02-21 21:58:16 +0530

    Clean up remove prop callback

--- src/components/wiki/WikiItem.js
+++ src/components/wiki/WikiItem.js
@@ -1,23 +1,20 @@
 import React from "react";
 
-const WikiItem = ({ item,index,currentIndex,show}) => {
-
-  const onTitleClick = idx => {
-     currentIndex(idx);
-  }
-
+const WikiItem = ({ item, index, currentIndex, show }) => {
   return (
     <React.Fragment key={index}>
       <div className="accordion-item">
         <h2 className="accordion-header" id={`heading${index}`}>
-          <button className="accordion-button" type="button" onClick={()=>onTitleClick(index)}>
+          <button
+            className="accordion-button"
+            type="button"
+            onClick={() => currentIndex(index)}
+          >
             {item.title}
           </button>
         </h2>
         <div id="collapseOne" className={`accordion-collapse ${show}`}>
-          <div className="accordion-body">
-            {item.content}
-          </div>
+          <div className="accordion-body">{item.content}</div>
         </div>
       </div>
     </React.Fragment>
