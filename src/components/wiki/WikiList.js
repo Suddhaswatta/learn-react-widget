43c3b1548a499229ec4539952cd6e9209c8b6059
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2022-02-22 17:01:39 +0530

    some docs

--- src/components/wiki/WikiList.js
+++ src/components/wiki/WikiList.js
@@ -10,8 +10,10 @@ const WikiList = () => {
   const [show, setShow] = useState(0); // State Array for index of selected accordion
 
   useEffect(() => {
+
+    /**Search on initial render  */
     const search = async () => {
-      const { data } = await WikiApi.get('',{
+      const { data } = await WikiApi.get("/", {
         params: {
           action: "query",
           list: "search",
@@ -30,11 +32,12 @@ const WikiList = () => {
       }
     }, 1000);
 
+    /**Use effect clean up func */
     return () => {
       console.log("Clean up");
       clearTimeout(timerId);
     };
-  }, [debouncedTerm]);
+  }, [debouncedTerm]); /** Use effect gets called after debounced term gets updated */ 
 
   const wikiItems = items.map((item, idx) => {
     const active = show === idx ? "show" : "collapse";
@@ -54,7 +57,10 @@ const WikiList = () => {
       <div className="container" id="wiki">
         <div className="row align-items-center">
           <div className="col-lg-12 col-md-12 col-sm-12">
-            <SearchWiki setSearchTerm={setDebouncedTerm} searchTerm={debouncedTerm} />
+            <SearchWiki
+              setSearchTerm={setDebouncedTerm}
+              searchTerm={debouncedTerm}
+            />
           </div>
         </div>
         <p></p>
