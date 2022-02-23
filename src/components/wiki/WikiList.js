607227d240ffa21a2c187c11c20b911bca587698
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2022-02-23 21:34:08 +0530

    Implement Custom hooks

--- src/components/wiki/WikiList.js
+++ src/components/wiki/WikiList.js
@@ -1,44 +1,30 @@
 import React, { useState, useEffect } from "react";
-import WikiApi from "../../api/WikiApi";
-import SearchWiki from "./SearchWiki";
 
+import useResults from "../../hooks/useResults";
+import SearchWiki from "./SearchWiki";
 import "./Wiki.css";
 import WikiItem from "./WikiItem";
+
 const WikiList = () => {
-  const [items, setItems] = useState([]);
   const [searchTerm, setSearchTerm] = useState("programming"); // State Array for Search Term data is received through callback
   const [show, setShow] = useState(0); // State Array for index of selected accordion
+  const [debouncedTerm, setDebouncedTerm] = useState(searchTerm);
+
+  const [results] = useResults(debouncedTerm); // Custom Hooks to fetch Wikipedia search results
 
   useEffect(() => {
     /**Search on initial render  */
-    const search = async () => {
-      const { data } = await WikiApi.get("", {
-        params: {
-          action: "query",
-          list: "search",
-          origin: "*",
-          format: "json",
-          srsearch: searchTerm,
-        },
-      });
-      console.log(data.query.search);
-      setItems(data.query.search);
-    };
-
     const timerId = setTimeout(() => {
-      if (searchTerm) {
-        search();
-      }
-    }, 1000);
+      setDebouncedTerm(searchTerm); // Debouncing
+    }, 10000);
 
     /**Use effect clean up func */
     return () => {
-      console.log("Clean up");
       clearTimeout(timerId);
     };
-  }, [searchTerm]); /** Use effect gets called after debounced term gets updated */
+  }, [searchTerm]); /** Use effect gets called after search term gets updated */
 
-  const wikiItems = items.map((item, idx) => {
+  const wikiItems = results.map((item, idx) => {
     const active = show === idx ? "show" : "collapse";
     return (
       <WikiItem
@@ -56,10 +42,7 @@ const WikiList = () => {
       <div className="container" id="wiki">
         <div className="row">
           <div className="col-lg-6 col-md-12 col-sm-12">
-            <SearchWiki
-              setSearchTerm={setSearchTerm}
-              searchTerm={searchTerm}
-            />
+            <SearchWiki setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
           </div>
         </div>
         <p></p>
